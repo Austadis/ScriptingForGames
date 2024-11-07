7aef20f2345c5ee129d12245080e7176bf24448c
Isaac Austad <ikeaustad@gmail.com>
2024-11-06 19:44:49 -0700

    Added Health Bar/decreasing health

--- AdventureGame/My project/Assets/Scripts/SimpleTriggerEventBehaviour.cs
+++ AdventureGame/My project/Assets/Scripts/SimpleTriggerEventBehaviour.cs
@@ -4,11 +4,9 @@ using UnityEngine.Events;
 public class SimpleTriggerEventBehaviour : MonoBehaviour
 {
     public UnityEvent triggerEvent;
-
     private void OnTriggerEnter(Collider other)
-    {
-        // Trigger the event and test with a debug message
-        triggerEvent.Invoke();
-        Debug.Log("Player interacted with the object!");
-    }
+        {
+            triggerEvent.Invoke();
+            Debug.Log("Player interacted with the object!");
+        }
  }
